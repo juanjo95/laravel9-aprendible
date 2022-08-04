cef54f3ddbbdc0d222d8abf7dda4a2048c381d0f
Juan J Salgado <zallagox95@hotmail.com>
2022-08-04 12:11:36 -0500

    feat 🚀: Rutas con nombre

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,12 @@
     <title>Inicio</title>
 </head>
 <body>
+    <ul>
+        <li><a href="{{ route('home') }}">Home</a></li>
+        <li><a href="{{ route('blog') }}">Blog</a></li>
+        <li><a href="{{ route('about') }}">Acerca de mi</a></li>
+        <li><a href="{{ route('contact') }}">Contacto</a></li>
+    </ul>
     <h1>Inicio</h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 //http://127.0.0.1:8000/blog => blog
 //http://127.0.0.1:8000/acerca-de-mi => about
 
-Route::view('/','welcome');
-Route::view('/contacto','contact');
-Route::view('/blog','blog');
-Route::view('/acerca-de-mi','about');
+Route::view('/','welcome')->name('home');
+Route::view('/contacto','contact')->name('contact');
+Route::view('/blog','blog')->name('blog');
+Route::view('/acerca-de-mi','about')->name('about');

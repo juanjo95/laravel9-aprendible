927833a486877910d09adaad107be1b70d9fb8de
Juan J Salgado <zallagox95@hotmail.com>
2022-08-04 13:32:51 -0500

    feat 🚀: Blade, el motor de plantillas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,12 +7,7 @@
     <title>Inicio</title>
 </head>
 <body>
-    <ul>
-        <li><a href="{{ route('home') }}">Home</a></li>
-        <li><a href="{{ route('blog') }}">Blog</a></li>
-        <li><a href="{{ route('about') }}">Acerca de mi</a></li>
-        <li><a href="{{ route('contact') }}">Contacto</a></li>
-    </ul>
+    @include('partials.navigation')
     <h1>Inicio</h1>
 </body>
 </html>

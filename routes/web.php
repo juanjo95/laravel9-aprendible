0bcb0998967f6487d0186284a760818a0cd4107c
Juan J Salgado <zallagox95@hotmail.com>
2022-08-04 11:58:14 -0500

    feat 🚀: Rutas

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+//http://127.0.0.1:8000/ => welcome
+//http://127.0.0.1:8000/contacto => contact
+//http://127.0.0.1:8000/blog => blog
+//http://127.0.0.1:8000/acerca-de-mi => about
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/','welcome');
+Route::view('/contacto','contact');
+Route::view('/blog','blog');
+Route::view('/acerca-de-mi','about');

4e4a70a16ac92cca4b85423c26eeb0216d7a963b
Juan J Salgado <zallagox95@hotmail.com>
2022-08-05 10:05:11 -0500

    feat 🚀: Conectar con bd usando clase DB:table()

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -2,6 +2,6 @@
 <x-layouts.app title="Blog" meta-description="Blog meta description">
     <h1>Blog</h1>
     @foreach ($posts as $post)
-        <h2>{{ $post['title'] }}</h2>
+        <h2>{{ $post->title }}</h2>
     @endforeach
 </x-layouts.app>
